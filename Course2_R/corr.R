f31e7f7990489518c16501188f95a9181531eb1b
milindaj <milindaj@gmail.com>
2014-09-09 07:52:23 +0530

    updated code for pollutant assignment 3

--- Course2_R/corr.R
+++ Course2_R/corr.R
@@ -20,21 +20,23 @@ corr <- function(directory, threshold = 10) {
   for (i in seq_along(csvList)) {
     csvData <- read.csv(csvList[i])
     if (nrow(csvData[complete.cases(csvData), ]) > threshold){
-      print(paste("****** ", csvList[i]))
-      bad <- is.na(csvData [, "nitrate"])
-      goodNData <- csvData [!bad, ] [, "nitrate"]
+      ##print(paste("****** ", csvList[i]))
+      ##bad <- is.na(csvData [, "nitrate"])
+      ##goodNData <- csvData [!bad, ] [, "nitrate"]
+      goodNData <- csvData [, "nitrate"]
 
-      print(str(goodNData))
-      print("******* print nitrate data ************")      
-      print(goodNData[1:10])
+      ##print(str(goodNData))
+      ##print("******* print nitrate data ************")      
+      ##print(goodNData[1:10])
       
-      bad <- is.na(csvData [, "sulfate"])
-      goodSData <- csvData [!bad, ] [, "sulfate"]  
-      print("******* print sulfate data ************")
-      print(goodSData[1:10])
-       print("nrow of goodNData = ", )
+      ##bad <- is.na(csvData [, "sulfate"])
+      ##goodSData <- csvData [!bad, ] [, "sulfate"]  
+      goodSData <- csvData [, "sulfate"]  
+      ##print("******* print sulfate data ************")
+      ##print(goodSData[1:10])
+      ##print("nrow of goodNData = ", )
       if (length(goodNData) == length(goodSData)) {
-        corrVector <- c(corrVector, cor(goodNData, goodSData))        
+        corrVector <- c(corrVector, cor(goodNData, goodSData, use = "na.or.complete"))        
       }
       
     }
