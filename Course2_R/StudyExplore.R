425f114279b4b2c4a3b1f53fae85b73d1efb2668
milindaj <milindaj@gmail.com>
2014-10-09 17:19:19 +0530

    Update StudyExplore.R

--- Course2_R/StudyExplore.R
+++ Course2_R/StudyExplore.R
@@ -94,4 +94,101 @@ with(airquality, {
   mtext("Multiple plots", outer=TRUE)
 })
 
+## course project - hopuse hold consumption
+hpcDT <- fread("household_power_consumption.txt", sep=";", colClasses = c("charecter", "charecter", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings=c("?"))
+
+
+
+## lattice
+
+library(lattice)
+xyplot(Ozone ~ Wind, data = airquality)
+
+airquality <- transform(airquality, Month = factor(Month))
+xyplot(Ozone ~ Wind | Month, data = airquality, layout=c(5,1))
+
+set.seed(10)
+x <- rnorm(100)
+f <- rep(0:1, each = 50)
+y <- x + f *x + rnorm(100, sd = 0.5)
+f <- factor(f, labels = c("Group 1", "Group2"))
+xyplot(y ~ x | f, layout=c(2,1))
+
+xyplot(y ~ x | f, panel = function(x, y, ...) {
+  panel.xyplot(x, y, ...)
+  panel.abline(h = median(y), lty = 2)
+})
+
+xyplot(y ~ x | f, panel = function(x, y, ...) {
+  panel.xyplot(x, y, ...)
+  panel.lmline(x, y, col = 2)
+})
+
+
+## ggplot2
+
+qplot(displ, hwy, data = mpg)
+
+qplot(displ, hwy, data = mpg, color = drv)
+
+qplot(displ, hwy, data = mpg, geom= c("point", "smooth"))
+
+qplot(hwy, data = mpg, fill = drv)
+
+
+# facets
+
+qplot(displ, hwy, data = mpg, facets = . ~drv)
+
+qplot(hwy, data = mpg, facets = drv~., binwidth = 2)
+
+qplot(hwy, data = mpg, facets = drv~cyl, binwidth = 2)
+
+qplot(displ, hwy, data = mpg, facets = . ~ drv, geom = c("point", "smooth"), method = "lm" )
+
+# ggplot
+g <- ggplot(mpg, aes(displ, hwy))
+p <- g + geom_point()
+
+p <- g + geom_point() + geom_smooth()
+
+p <- g + geom_point() + geom_smooth(method = "lm")
+
+
+p <- g + geom_point() + facet_grid(. ~ drv) + geom_smooth(method = "lm")
+
+
+g + geom_point(color = "steelblue", size = 4, alpha = 1/2)
+
+g + geom_point(aes(color = drv), size = 4, alpha = 1/2)
+
+
+g + geom_point(aes(color = drv), size = 4, alpha = 1/2) + labs(title = " car chart") + labs(x = "my way", y = "highway")
+
+
+g + geom_point(aes(color = drv), size = 4, alpha = 1/2) + geom_smooth (size = 4, linetype = 3, method = "lm", se = FALSE)
+
+
+g + geom_point(aes(color = drv), size = 4, alpha = 1/2) + labs(title = " car chart") + labs(x = "my way", y = "highway") + theme_bw(base_family = "Times")
+
+
+# outliers
+testdata <- data.frame (x = 1:100, y = rnorm(100))
+testdata[50,2] <- 100
+
+plot(testdata$x, testdata$y, type = "1", ylim=c(-3,3))
+
+g <- ggplot(testdata, aes(x = x, y = y))
+g + geom_line()
+g + geom_line() + coord_cartesian(ylim = c(-3, 3))
+
+
+g <- ggplot(mpg, aes(displ, hwy))
+g <- g + geom_point(alpha = 1/3) 
+g <- g + facet_wrap (drv ~ cyl, nrow = 4, ncol = 4) 
+g <- g + geom_smooth (method = "lm", se = FALSE, col = "steelblue") 
+g <- g + theme_bw(base_size = 12) 
+g <- g + labs(x = "x label") 
+g <- g + labs(y = "y label") 
+g <- g + labs(title = "main title")
 
